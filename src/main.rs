d9c1498e5a655ae3377671e5eb8f6fd8366ded9c
swapsCAPS <verstegen.daan@gmail.com>
2020-08-30 22:38:12 +0200

    very serious work

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,36 @@
 #[macro_use] extern crate rocket;
 
 use rocket::http::RawStr;
-use voca_rs::*;
+use rocket_contrib::{
+    templates::Template,
+    serve::StaticFiles,
+};
+use voca_rs::case;
+use std::collections::HashMap;
 
 #[get("/")]
-fn root() -> &'static str {
-    "Kut man, echt jammer."
+fn root() -> Template {
+    let mut context = HashMap::new();
+    context.insert("main", "Kut man. Echt jammer.");
+    Template::render("index", &context)
 }
 
 #[get("/<ding>")]
-fn ding(ding: &RawStr) -> String {
+fn ding(ding: &RawStr) -> Template {
     let capitalized = case::capitalize(ding.as_str(), true);
-    format!("{}... mooi kut man", capitalized)
+    let mut context = HashMap::new();
+    context.insert("main", format!("{}... Mooi kut.", capitalized));
+    Template::render("index", &context)
 }
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![
-        root,
-        ding,
-    ])
+    rocket::ignite()
+        .mount("/", routes![
+            root,
+            ding,
+        ])
+        .mount("/public", StaticFiles::from("./public"))
+        .attach(Template::fairing())
 }
 
 fn main() {
@@ -31,19 +43,12 @@ fn main() {
 mod test {
     use super::rocket;
     use rocket::local::Client;
-    use rocket::http::Status;
-
-    #[test]
-    fn root() {
-        let client = Client::new(rocket()).expect("is rocket");
-        let mut response = client.get("/").dispatch();
-        assert_eq!(response.body_string(), Some("Kut man, echt jammer.".into()));
-    }
 
     #[test]
     fn ding() {
         let client = Client::new(rocket()).expect("is rocket");
         let mut response = client.get("/regen").dispatch();
         assert_eq!(response.body_string(), Some("Regen... mooi kut man".into()));
+
     }
 }
